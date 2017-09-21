7896d210317ca84bcccb44bc19e050dcd4dd7ed1
Austin Joyal <theausome@outlook.com>
2017-09-21 22:23:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import threading
+from Queue import Queue
+from spider import Spider
+from getdomain import *
+from general import *
+print 'start'
+PROJECT_NAME = 'theausome'
+HOMEPAGE = raw_input('Enter the website address')
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 3
+print 'where'
+queue = Queue()
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+
+# Create worker threads (will die when the main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True                    # Make sure that workers die when the main exits
+        t.start()
+
+
+# Do the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+
+# Each queued_links is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+
+# Checks if the queue file is empty. If not, find the links in it.
+def crawl():
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links) > 0:
+        print str(len(queued_links)), ' links are in the queue.'
+        create_jobs()
+
+
+create_workers()
+crawl()
